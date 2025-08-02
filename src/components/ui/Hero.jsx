9844b2f9fcf1a0940a4a2f9dd33a93deb54746c1
adnan <adnankaka.0092@gmail.com>
2025-08-02 22:47:30 +0500

    commit

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = ({ title, text }) => {
     >
       <div className="p-[clamp(5.625rem,1.8662rem_+_4.6948vw,7.5rem)_0_clamp(9.6875rem,4.0493rem_+_7.0423vw,12.5rem)] backface-hidden text-center">
         <h1 className="text-white mb-2.5">{title}</h1>
-        <p className="text-white/90 lg:w-3/4 mx-auto">{text}</p>
+        <p className="text-white/90 lg:w-3/4 xl:w-[60%] mx-auto">{text}</p>
       </div>
     </div>
   );
